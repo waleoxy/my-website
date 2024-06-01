c6a1e6b95b24af73682d5999a9cc68415fb288d7
olawale otubu <waleoxy@gmail.com>
2024-06-01 11:35:45 +0100

    website all commit

---
+++ components/grid.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
+import { gridItems } from "@/data";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            className,
+            description,
+            id,
+            img,
+            imgClassName,
+            spareImg,
+            title,
+            titleClassName,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              description={description}
+              title={title}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              spareImg={spareImg}
+              titleClassName={titleClassName}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
